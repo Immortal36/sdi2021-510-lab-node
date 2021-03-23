7db582864ca3f7b05b40bddb5940068a2fb973f3
Immortal <ignacio-oviedo@hotmail.com>
2021-03-23 12:57:35 +0100

    SDI-2021-510-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,27 +1,41 @@
-module.exports = function(app) {
-    app.get("/canciones", function(req, res) {
-        let respuesta = "";
-
-        if(req.query.nombre != null)
-            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
-
-        if(typeof (req.query.autor) != "undefined")
-            respuesta += + 'Autor: '+ req.query.autor;
+module.exports = function(app, swig) {
+    app.get("/canciones", function (req, res) {
+        let canciones = [{
+            "nombre": "Blank space",
+            "precio": "1.2"
+        }, {
+            "nombre": "See you again",
+            "precio": "1.3"
+        }, {
+            "nombre": "Uptown Funk",
+            "precio": "1.1"
+        }];
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor: 'Tienda de canciones',
+            canciones: canciones
+        });
 
         res.send(respuesta);
     });
 
-    app.get('/suma', function(req, res) {
+    app.get('/suma', function (req, res) {
         let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
         res.send(String(respuesta));
     });
 
-    app.get('/canciones/:id', function(req, res) {
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    });
+
+    app.get('/canciones/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
 
-    app.get('/canciones/:genero/:id', function(req, res) {
+    app.get('/canciones/:genero/:id', function (req, res) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
@@ -36,5 +50,11 @@ module.exports = function(app) {
     app.get('/promo*', function (req, res) {
         res.send('Respuesta patrón promo* ');
     });
+
+    //Codigo dado incompleto
+    //module.exports = function(app, swig) {
+    //    app.get("/nuevas/canciones", function(req, res) {
+    //        var canciones = [ {
+    //            "nombre" : "Blank space",
 
 };
