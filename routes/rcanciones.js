815caff024dcfab2c1a4f3968d9084cae81ca4a6
Immortal <ignacio-oviedo@hotmail.com>
2021-04-05 11:53:04 +0200

    SDI-2021-510-8.1-Acceso a datos de Mongo desde Node.js.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,4 +1,4 @@
-module.exports = function(app, swig) {
+module.exports = function(app, swig, mongo) {
     app.get('/canciones', function (req, res) {
         let canciones = [{
             "nombre": "Blank space",
@@ -16,6 +16,31 @@ module.exports = function(app, swig) {
         });
 
         res.send(respuesta);
+    });
+
+    app.post('/cancion', function (req, res) {
+        let cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio
+        }
+
+        // Conectarse
+        mongo.MongoClient.connect(app.get('db'), function(err, db) {
+            if (err) {
+                res.send("Error de conexión: " + err);
+            } else {
+                let collection = db.collection('canciones');
+                collection.insertOne(cancion, function(err, result) {
+                    if (err) {
+                        res.send("Error al insertar " + err);
+                    } else {
+                        res.send("Agregada id: "+ result.ops[0]._id);
+                    }
+                    db.close();
+                });
+            }
+        });
     });
 
     app.get('/suma', function (req, res) {
@@ -39,12 +64,6 @@ module.exports = function(app, swig) {
         let respuesta = 'id: ' + req.params.id + '<br>'
             + 'Género: ' + req.params.genero;
         res.send(respuesta);
-    });
-
-    app.post('/cancion', function (req, res) {
-        res.send("Cancion agregada: " + req.body.nombre + "<br>"
-            + " genero: " + req.body.genero + "<br>"
-            + " precio: " + req.body.precio);
     });
 
     app.get('/promo*', function (req, res) {
